cfd766fd84b478d590a7e6bc974b12c19afd3dee
YOOJUNG\user <yoojunging@naver.com>
2025-04-12 17:01:24 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -59,5 +59,6 @@ def convert_pdf():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
