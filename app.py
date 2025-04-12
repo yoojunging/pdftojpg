7d3f4e097786623cbdf630328e44b1a33f5a9c29
YOOJUNG\user <yoojunging@naver.com>
2025-04-12 16:57:53 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -58,6 +58,6 @@ def convert_pdf():
         'zip_url': zip_url
     })
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
